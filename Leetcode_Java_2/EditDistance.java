0a3702e1882468d1c70592f8427f54018d44244b
Shan Ye <sye@usfca.edu>
2014-04-07 23:59:39 -0800

    Edit Distance

---
+++ Leetcode_Java_2/EditDistance.java
@@ -0,0 +1,32 @@
+class EditDistance {
+    public int minDistance(String word1, String word2) {
+        int row = word1.length() + 1;
+        int col = word2.length() + 1;
+
+        int[][] dp = new int[row][col];
+
+        for (int i = 0; i < row; i++) {
+            dp[i][0] = i;
+        }
+
+        for (int j = 0; j < col; j++) {
+            dp[0][j] = j;
+        }
+
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < col; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    int replace = dp[i - 1][j - 1] + 1;
+                    int add = dp[i][j - 1] + 1;
+                    int del = dp[i - 1][j] + 1;
+
+                    dp[i][j] = Math.min(Math.min(add, del), replace);
+                }
+            }
+        }
+
+        return dp[row - 1][col - 1];
+    }
+}
