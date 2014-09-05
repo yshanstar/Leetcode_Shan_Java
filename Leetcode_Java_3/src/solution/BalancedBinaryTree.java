fa698b7619278a32ecb4d9f7bd862926a45f898a
shye <shye@microsoft.com>
2014-09-04 23:36:13 -0700

    Balanced Binary Tree

---
+++ Leetcode_Java_3/src/solution/BalancedBinaryTree.java
@@ -0,0 +1,32 @@
+package solution;
+
+import Util.TreeNode;
+
+public class BalancedBinaryTree {
+	public boolean isBalanced(TreeNode root) {
+		if (root == null || (root.left == null && root.right == null)) {
+			return true;
+		}
+		int leftDepth = height(root.left);
+		int rightDepth = height(root.right);
+
+		if (Math.abs(leftDepth - rightDepth) > 1) {
+			return false;
+		}
+
+		return isBalanced(root.left) && isBalanced(root.right);
+
+	}
+
+	private int height(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+
+		if (root.left == null && root.right == null) {
+			return 1;
+		}
+
+		return Math.max(height(root.left), height(root.right)) + 1;
+	}
+}
