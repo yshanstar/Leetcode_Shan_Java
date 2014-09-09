6862d2de4e4e21946f8fb5cb9f8c5ee5bf9f90a5
shye <shye@microsoft.com>
2014-09-08 23:28:48 -0700

    Sort Colors

---
+++ Leetcode_Java_3/src/solution/SortColors.java
@@ -0,0 +1,25 @@
+package solution;
+
+public class SortColors {
+	public void sortColors(int[] A) {
+		if (A.length == 0) {
+			return;
+		}
+		int redIdx = -1;
+		int blueIdx = A.length;
+		for (int i = 0; i < A.length; i++) {
+			if (A[i] == 0) {
+				redIdx++;
+				int tmp = A[redIdx];
+				A[redIdx] = A[i];
+				A[i] = tmp;
+			} else if (A[i] == 2) {
+				blueIdx--;
+				int tmp = A[blueIdx];
+				A[blueIdx] = A[i];
+				A[i] = tmp;
+				i--;
+			}
+		}
+	}
+}
