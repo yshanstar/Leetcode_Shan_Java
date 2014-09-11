15bd5edc08844e352dd04eeceff3974d2d84c4fc
shye <shye@microsoft.com>
2014-09-11 15:11:54 -0700

    Copy List with Random Pointer

---
+++ Leetcode_Java_3/src/util/RandomListNode.java
@@ -0,0 +1,12 @@
+package util;
+
+public class RandomListNode {
+	public int label;
+	public RandomListNode next, random;
+
+	public RandomListNode(int value) {
+		this.label = value;
+		next = null;
+		random = null;
+	}
+}
