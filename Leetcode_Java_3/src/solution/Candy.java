6d63954ded0f3ef485dbe5b3a63c0a6e9268fe8e
Shan Ye <yshanstar1988@gmail.com>
2014-09-18 22:21:24 -0700

    Candy

---
+++ Leetcode_Java_3/src/solution/Candy.java
@@ -0,0 +1,29 @@
+package solution;
+
+public class Candy {
+	public int candy(int[] ratings) {
+		if (ratings.length == 0) {
+			return 0;
+		}
+		int[] candies = new int[ratings.length];
+
+		candies[0] = 1;
+		for (int i = 1; i < ratings.length; i++) {
+			if (ratings[i] > ratings[i - 1]) {
+				candies[i] = candies[i - 1] + 1;
+			} else {
+				candies[i] = 1;
+			}
+		}
+
+		int sum = 0;
+		for (int i = ratings.length - 1; i > 0; i--) {
+			if (ratings[i - 1] > ratings[i] && candies[i - 1] <= candies[i]) {
+				candies[i - 1] = candies[i] + 1;
+			}
+			sum += candies[i];
+		}
+		sum += candies[0];
+		return sum;
+	}
+}
