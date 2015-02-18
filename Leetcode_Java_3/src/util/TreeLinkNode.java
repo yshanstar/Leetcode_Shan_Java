d7a234bcfadce354602533b0ec141d694cad0635
shye <shye@microsoft.com>
2015-02-17 16:08:21 -0800

    Populating Next Right Pointers in Each Node

---
+++ Leetcode_Java_3/src/util/TreeLinkNode.java
@@ -0,0 +1,13 @@
+package util;
+
+public class TreeLinkNode {
+	public int val;
+	public TreeLinkNode left, right, next;
+
+	public TreeLinkNode(int v) {
+		this.val = v;
+		this.left = null;
+		this.right = null;
+		this.next = null;
+	}
+}
