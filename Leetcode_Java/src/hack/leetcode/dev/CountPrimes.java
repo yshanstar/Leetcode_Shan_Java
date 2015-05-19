13d14451cf20c3ab2a648f97fb5f01445463beea
Shan Ye <shye@microsoft.com>
2015-05-18 22:05:15 -0700

    Count Primes

---
+++ Leetcode_Java/src/hack/leetcode/dev/CountPrimes.java
@@ -0,0 +1,35 @@
+package hack.leetcode.dev;
+
+/*
+ * Count the number of prime numbers less than a non-negative number, n
+ */
+public class CountPrimes {
+	public int countPrimes(int n) {
+		// Sieve of Eratosthenes.
+		if (n <= 2) {
+			return 0;
+		}
+
+		boolean[] primes = new boolean[n];
+		for (int i = 2; i < n; i++) {
+			primes[i] = true;
+		}
+
+		for (int i = 2; i <= Math.sqrt(n - 1); i++) {
+			if (primes[i]) {
+				for (int j = i + i; j < n; j += i) {
+					primes[j] = false;
+				}
+			}
+		}
+
+		int count = 0;
+		for (int i = 2; i < n; i++) {
+			if (primes[i]) {
+				count++;
+			}
+		}
+
+		return count;
+	}
+}
