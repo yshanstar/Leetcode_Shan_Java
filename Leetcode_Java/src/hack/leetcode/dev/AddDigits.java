e2d1332489b91e0411c59eea38d853ae1953104d
shye <shye@microsoft.com>
2015-08-17 15:38:05 -0700

    Add Digits

---
+++ Leetcode_Java/src/hack/leetcode/dev/AddDigits.java
@@ -0,0 +1,14 @@
+package hack.leetcode.dev;
+
+/*
+ * Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+ For example:
+
+ Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+ */
+public class AddDigits {
+	public int addDigits(int num) {
+		return 1 + (num - 1) % 9;
+	}
+}
