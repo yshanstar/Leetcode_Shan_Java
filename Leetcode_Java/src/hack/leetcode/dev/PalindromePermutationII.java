d8d45fb2c5e51fee4a373869fa93b00678d9bac5
shye <shye@microsoft.com>
2015-10-20 12:32:08 -0700

    Palindrome Permutation II

---
+++ Leetcode_Java/src/hack/leetcode/dev/PalindromePermutationII.java
@@ -0,0 +1,84 @@
+package hack.leetcode.dev;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/*
+ * Given a string s, return all the palindromic permutations (without duplicates) of it. Return an empty list if no palindromic permutation could be form.
+
+ For example:
+
+ Given s = "aabb", return ["abba", "baab"].
+
+ Given s = "abc", return [].
+ */
+public class PalindromePermutationII {
+	public List<String> generatePalindromes(String s) {
+		if (s == null) {
+			throw new IllegalArgumentException("s is null");
+		}
+
+		List<String> res = new ArrayList<String>();
+		int len = s.length();
+		if (len == 0) {
+			return res;
+		}
+
+		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+		for (char c : s.toCharArray()) {
+			if (map.containsKey(c)) {
+				map.put(c, map.get(c) + 1);
+			} else {
+				map.put(c, 1);
+			}
+		}
+
+		int oddCount = 0;
+		char oddChar = 'a';
+
+		for (char c : map.keySet()) {
+			if (map.get(c) % 2 == 1) {
+				oddCount++;
+				oddChar = c;
+			}
+		}
+
+		if (oddCount >= 2) {
+			return res;
+		}
+
+		if (oddCount == 1) {
+			helper(map, oddChar + "", len, res);
+		} else {
+			helper(map, "", len, res);
+		}
+
+		return res;
+	}
+
+	private void helper(HashMap<Character, Integer> map, String cur,
+			int target, List<String> res) {
+		String newCur = cur;
+		int len = newCur.length();
+
+		if (len == target) {
+			res.add(newCur);
+			return;
+		}
+
+		for (char c : map.keySet()) {
+			if (map.get(c) >= 2) {
+				newCur = c + cur + c;
+				map.put(c, map.get(c) - 2);
+				helper(map, newCur, target, res);
+				map.put(c, map.get(c) + 2);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		PalindromePermutationII test = new PalindromePermutationII();
+		test.generatePalindromes("a");
+	}
+}
