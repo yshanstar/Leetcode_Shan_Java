a627d0ab7304cd067c7ea2dc68801e01c816894f
shye <shye@microsoft.com>
2016-03-29 10:36:41 -0700

    Gray Code

---
+++ Leetcode_Java_3/src/solution/GreyCode.java
@@ -0,0 +1,40 @@
+package solution;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * The gray code is a binary numeral system where two successive values differ in only one bit.
+
+ Given a non-negative integer n representing the total number of bits in the code, print the sequence of gray code. A gray code sequence must begin with 0.
+
+ For example, given n = 2, return [0,1,3,2]. Its gray code sequence is:
+
+ 00 - 0
+ 01 - 1
+ 11 - 3
+ 10 - 2
+ Note:
+ For a given n, a gray code sequence is not uniquely defined.
+
+ For example, [0,2,3,1] is also a valid gray code sequence according to the above definition.
+ */
+public class GreyCode {
+	public List<Integer> grayCode(int n) {
+		List<Integer> res = new ArrayList<Integer>();
+
+		if (n < 0) {
+			return res;
+		}
+
+		res.add(0);
+		for (int i = 0; i < n; i++) {
+			int inc = 1 << i;
+			for (int j = res.size() - 1; j >= 0; j--) {
+				res.add(res.get(j) + inc);
+			}
+		}
+
+		return res;
+	}
+}
