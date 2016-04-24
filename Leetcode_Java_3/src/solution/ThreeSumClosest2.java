714b9b39357695184ea3d9b6642820c18be1270c
Shan Ye <sye@dons.usfca.edu>
2016-04-23 22:34:13 -0700

    3Sum Closest

---
+++ Leetcode_Java_3/src/solution/ThreeSumClosest2.java
@@ -0,0 +1,49 @@
+package solution;
+
+import java.util.Arrays;
+
+/*
+ * Given an array S of n integers, find three integers in S such that the sum is closest to a given number, target. Return the sum of the three integers. You may assume that each input would have exactly one solution.
+
+    For example, given array S = {-1 2 1 -4}, and target = 1.
+
+    The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+
+ */
+public class ThreeSumClosest2 {
+	public int threeSumClosest(int[] nums, int target) {
+		if (nums == null || nums.length == 0) {
+			return 0;
+		}
+
+		int cloest = Integer.MAX_VALUE;
+		int res = Integer.MAX_VALUE;
+
+		Arrays.sort(nums);
+
+		for (int i = 0; i < nums.length - 2; i++) {
+			int low = i + 1;
+			int high = nums.length - 1;
+
+			while (low < high) {
+				int sum = nums[i] + nums[low] + nums[high];
+				int diff = target - sum;
+
+				if (diff == 0) {
+					return sum;
+				} else if (diff > 0) {
+					low++;
+				} else {
+					high--;
+				}
+
+				if (Math.abs(diff) < cloest) {
+					cloest = Math.abs(diff);
+					res = sum;
+				}
+			}
+		}
+
+		return res;
+	}
+}
