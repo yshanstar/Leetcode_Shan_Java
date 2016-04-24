1ea7fd29c3d3681a1658d56ee6ff94aa3dbd2112
Shan Ye <sye@dons.usfca.edu>
2016-04-23 23:27:11 -0700

    Populating Next Right Pointers in Each Node

---
+++ Leetcode_Java_3/src/solution/PopulatingNextRightPointersEachNode.java
@@ -0,0 +1,73 @@
+package solution;
+
+import util.TreeLinkNode;
+
+/*
+ * Given a binary tree
+
+    struct TreeLinkNode {
+      TreeLinkNode *left;
+      TreeLinkNode *right;
+      TreeLinkNode *next;
+    }
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+
+Note:
+
+You may only use constant extra space.
+You may assume that it is a perfect binary tree (ie, all leaves are at the same level, and every parent has two children).
+For example,
+Given the following perfect binary tree,
+         1
+       /  \
+      2    3
+     / \  / \
+    4  5  6  7
+After calling your function, the tree should look like:
+         1 -> NULL
+       /  \
+      2 -> 3 -> NULL
+     / \  / \
+    4->5->6->7 -> NULL
+ */
+public class PopulatingNextRightPointersEachNode {
+	public void connect(TreeLinkNode root) {
+		if (root == null || (root.left == null && root.right == null)) {
+			return;
+		}
+
+		TreeLinkNode left = root.left;
+		TreeLinkNode right = root.right;
+
+		while (left != null) {
+			left.next = right;
+			if (right != null) {
+				right = right.left;
+			}
+			left = left.right;
+		}
+
+		connect(root.left);
+		connect(root.right);
+	}
+
+	public void connect2(TreeLinkNode root) {
+		if (root == null) {
+			return;
+		}
+
+		while (root.left != null) {
+			TreeLinkNode tmp = root;
+			while (tmp != null) {
+				tmp.left.next = tmp.right;
+				if (tmp.next != null) {
+					tmp.right.next = tmp.next.left;
+				}
+				tmp = tmp.next;
+			}
+			root = root.left;
+		}
+	}
+}
