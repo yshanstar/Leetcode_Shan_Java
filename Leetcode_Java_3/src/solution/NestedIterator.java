6193fb42b4fd89b97cc21473c3289a4253ec5964
shye <shye@microsoft.com>
2016-04-25 15:09:44 -0700

    Nested Iterator

---
+++ Leetcode_Java_3/src/solution/NestedIterator.java
@@ -0,0 +1,63 @@
+package solution;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Stack;
+
+import util.NestedInteger;
+
+/*
+ * Given a nested list of integers, implement an iterator to flatten it.
+
+ Each element is either an integer, or a list -- whose elements may also be integers or other lists.
+
+ Example 1:
+ Given the list [[1,1],2,[1,1]],
+
+ By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,1,2,1,1].
+
+ Example 2:
+ Given the list [1,[4,[6]]],
+
+ By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,4,6].
+ */
+public class NestedIterator implements Iterator<Integer> {
+	Stack<Iterator<NestedInteger>> stack;
+	Iterator<NestedInteger> cur;
+	NestedInteger next;
+
+	public NestedIterator(List<NestedInteger> nestedList) {
+		stack = new Stack<Iterator<NestedInteger>>();
+		stack.push(nestedList.iterator());
+	}
+
+	@Override
+	public Integer next() {
+		return next.getInteger();
+	}
+
+	@Override
+	public boolean hasNext() {
+		next = getNext();
+		return next != null;
+	}
+
+	private NestedInteger getNext() {
+		while (!stack.isEmpty()) {
+			cur = stack.peek();
+			while (cur.hasNext()) {
+				NestedInteger next = cur.next();
+				if (next.isInteger()) {
+					return next;
+				}
+				stack.push(next.getList().iterator());
+				cur = stack.peek();
+			}
+			if (!cur.hasNext()) {
+				stack.pop();
+			}
+		}
+
+		return null;
+	}
+}
