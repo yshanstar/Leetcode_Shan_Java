555b4af97aadd6f7d482a29777b01d85fda57296
shye <shye@microsoft.com>
2016-04-25 14:51:10 -0700

    Course Schedule

---
+++ Leetcode_Java_3/src/solution/CourseSchedule.java
@@ -0,0 +1,63 @@
+package solution;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+ * There are a total of n courses you have to take, labeled from 0 to n - 1.
+
+ Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+
+ Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
+
+ For example:
+
+ 2, [[1,0]]
+ There are a total of 2 courses to take. To take course 1 you should have finished course 0. So it is possible.
+
+ 2, [[1,0],[0,1]]
+ There are a total of 2 courses to take. To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
+
+ Note:
+ The input prerequisites is a graph represented by a list of edges, not adjacency matrices. Read more about how a graph is represented.
+ */
+public class CourseSchedule {
+	public boolean canFinish(int numCourses, int[][] prerequisites) {
+		if (prerequisites == null || prerequisites.length == 0 || numCourses == 0) {
+			return true;
+		}
+
+		int length = prerequisites.length;
+		int[] counter = new int[numCourses];
+
+		for (int[] prerequisite : prerequisites) {
+			counter[prerequisite[0]]++;
+		}
+
+		Queue<Integer> nonPreCourse = new LinkedList<Integer>();
+		for (int i = 0; i < counter.length; i++) {
+			if (counter[i] == 0) {
+				nonPreCourse.offer(i);
+			}
+		}
+
+		int numOfNonPre = nonPreCourse.size();
+
+		while (!nonPreCourse.isEmpty()) {
+			int course = nonPreCourse.poll();
+
+			for (int[] prerequisite : prerequisites) {
+				if (prerequisite[1] == course) {
+					counter[prerequisite[0]]--;
+
+					if (counter[prerequisite[0]] == 0) {
+						numOfNonPre++;
+						nonPreCourse.offer(prerequisite[0]);
+					}
+				}
+			}
+		}
+
+		return (numOfNonPre == numCourses);
+	}
+}
---
+++ Leetcode_Java_3/src/solution/CourseScheduleII.java
@@ -0,0 +1,82 @@
+package solution;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/*
+ * There are a total of n courses you have to take, labeled from 0 to n - 1.
+
+ Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+
+ Given the total number of courses and a list of prerequisite pairs, return the ordering of courses you should take to finish all courses.
+
+ There may be multiple correct orders, you just need to return one of them. If it is impossible to finish all courses, return an empty array.
+
+ For example:
+
+ 2, [[1,0]]
+ There are a total of 2 courses to take. To take course 1 you should have finished course 0. So the correct course order is [0,1]
+
+ 4, [[1,0],[2,0],[3,1],[3,2]]
+ There are a total of 4 courses to take. To take course 3 you should have finished both courses 1 and 2. Both courses 1 and 2 should be taken after you finished course 0. So one correct course order is [0,1,2,3]. Another correct ordering is[0,2,1,3].
+ */
+public class CourseScheduleII {
+	public int[] findOrder(int numCourses, int[][] prerequisites) {
+		int[] res = new int[numCourses];
+
+		if (numCourses == 0) {
+			return res;
+		}
+
+		if (prerequisites == null || prerequisites.length == 0) {
+			for (int i = 0; i < numCourses; i++) {
+				res[i] = i;
+			}
+
+			return res;
+		}
+
+		int[] counter = new int[numCourses];
+		Queue<Integer> nonPreCourse = new LinkedList<Integer>();
+		List<Integer> tmpRes = new ArrayList<Integer>();
+
+		for (int[] prerequisite : prerequisites) {
+			counter[prerequisite[0]]++;
+		}
+
+		for (int i = 0; i < counter.length; i++) {
+			if (counter[i] == 0) {
+				nonPreCourse.offer(i);
+			}
+		}
+
+		// number of courses that have no prerequisites
+		int numNoPre = nonPreCourse.size();
+
+		while (!nonPreCourse.isEmpty()) {
+			int course = nonPreCourse.poll();
+			tmpRes.add(course);
+
+			for (int[] prerequisite : prerequisites) {
+				if (prerequisite[1] == course) {
+					counter[prerequisite[0]]--;
+					if (counter[prerequisite[0]] == 0) {
+						numNoPre++;
+						nonPreCourse.offer(prerequisite[0]);
+					}
+				}
+			}
+		}
+
+		if (numNoPre == numCourses) {
+			for (int i = 0; i < tmpRes.size(); i++) {
+				res[i] = tmpRes.get(i);
+			}
+			return res;
+		}
+
+		return new int[0];
+	}
+}
