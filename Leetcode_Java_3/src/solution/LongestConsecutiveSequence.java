fc44a263e9d26fc9e08c8491c53513c95dcf2f51
Shan Ye <sye@dons.usfca.edu>
2016-05-09 21:34:03 -0700

    Single Number II

--- Leetcode_Java_3/src/solution/LongestConsecutiveSequence.java
+++ Leetcode_Java_3/src/solution/LongestConsecutiveSequence.java
@@ -1,49 +1,9 @@
 package solution;
 
 import java.util.HashSet;
-import java.util.Set;
 
-/*
- * Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
-
- For example,
- Given [100, 4, 200, 1, 3, 2],
- The longest consecutive elements sequence is [1, 2, 3, 4]. Return its length: 4.
-
- Your algorithm should run in O(n) complexity.
- */
 public class LongestConsecutiveSequence {
-	public int longestConsecutive(int[] nums) {
-		Set<Integer> set = new HashSet<Integer>();
-
-		for (int i : nums) {
-			set.add(i);
-		}
-
-		int max = 0;
-
-		for (int num : nums) {
-			int left = num - 1;
-			int right = num + 1;
-			int count = 1;
-
-			while (set.contains(left)) {
-				set.remove(left--);
-				count++;
-			}
-
-			while (set.contains(right)) {
-				set.remove(right++);
-				count++;
-			}
-
-			max = Math.max(max, count);
-		}
-
-		return max;
-	}
-
-	public int longestConsecutive2(int[] num) {
+	public int longestConsecutive(int[] num) {
 		HashSet<Integer> numbers = new HashSet<Integer>();
 		for (int i : num) {
 			numbers.add(i);
--- Leetcode_Java_3/src/solution/SingleNumberII.java
+++ Leetcode_Java_3/src/solution/SingleNumberII.java
@@ -1,6 +1,30 @@
 package solution;
 
+/*
+ * Given an array of integers, every element appears three times except for one. Find that single one.
+
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+ */
 public class SingleNumberII {
+	public int singleNumber2(int[] nums) {
+		if (nums == null || nums.length == 0) {
+			return 0;
+		}
+		int[] count = new int[32];
+		int res = 0;
+		for (int i = 0; i < count.length; i++) {
+			for (int num : nums) {
+				if ((num >> i & 1) > 0) {
+					count[i]++;
+				}
+			}
+			res |= ((count[i] % 3)) << i;
+		}
+
+		return res;
+	}
+
 	public int singleNumber(int[] A) {
 		int[] count = new int[32];
 		int res = 0;
