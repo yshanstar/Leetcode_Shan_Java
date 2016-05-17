18590985d2bfcc524e597905d063ca693f40ebac
shye <shye@microsoft.com>
2016-05-16 17:47:34 -0700

    candy

--- Leetcode_Java_3/src/solution/Candy.java
+++ Leetcode_Java_3/src/solution/Candy.java
@@ -1,7 +1,46 @@
 package solution;
 
+import java.util.Arrays;
+
+/*
+ * There are N children standing in a line. Each child is assigned a rating value.
+
+ You are giving candies to these children subjected to the following requirements:
+
+ Each child must have at least one candy.
+ Children with a higher rating get more candies than their neighbors.
+ What is the minimum candies you must give?
+ */
 public class Candy {
-	public int candy(int[] ratings) {
+	public static int candy(int[] ratings) {
+		if (ratings == null || ratings.length == 0) {
+			return 0;
+		}
+
+		int[] candy = new int[ratings.length];
+		Arrays.fill(candy, 1);
+
+		for (int i = 1; i < ratings.length; i++) {
+			if (ratings[i] > ratings[i - 1]) {
+				candy[i] = candy[i - 1] + 1;
+			}
+		}
+
+		for (int i = ratings.length - 1; i > 0; i--) {
+			if (ratings[i - 1] > ratings[i] && candy[i - 1] <= candy[i]) {
+				candy[i - 1] = candy[i] + 1;
+			}
+		}
+
+		int res = 0;
+		for (int num : candy) {
+			res += num;
+		}
+
+		return res;
+	}
+
+	public int candy2(int[] ratings) {
 		if (ratings.length == 0) {
 			return 0;
 		}
@@ -25,5 +64,10 @@ public class Candy {
 		}
 		sum += candies[0];
 		return sum;
+	}
+
+	public static void main(String[] args) {
+		int[] ratings = new int[] { 1, 2, 3, 4, 5 };
+		candy(ratings);
 	}
 }
