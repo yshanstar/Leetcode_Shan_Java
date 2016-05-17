2734b8ecb846fa00f08a7dd38990c485f12b640f
shye <shye@microsoft.com>
2016-05-16 17:12:49 -0700

    Binary Tree Maximum Path Sum

--- Leetcode_Java_3/src/solution/BinaryTreeMaximumPathSum.java
+++ Leetcode_Java_3/src/solution/BinaryTreeMaximumPathSum.java
@@ -2,8 +2,62 @@ package solution;
 
 import util.TreeNode;
 
+/*
+ * Given a binary tree, find the maximum path sum.
+
+ For this problem, a path is defined as any sequence of nodes from some starting node to any node in the tree along the parent-child connections. The path does not need to go through the root.
+
+ For example:
+ Given the below binary tree,
+
+ 1
+ / \
+ 2   3
+ Return 6.
+ */
 public class BinaryTreeMaximumPathSum {
+	class Node {
+		int sum;
+
+		public Node(int v) {
+			this.sum = v;
+		}
+	}
+
 	public int maxPathSum(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+
+		if (root.left == null && root.right == null) {
+			return root.val;
+		}
+
+		Node maxNode = new Node(Integer.MIN_VALUE);
+		Node tmp = new Node(0);
+
+		helper(maxNode, tmp, root);
+
+		return maxNode.sum;
+	}
+
+	private void helper(Node maxNode, Node tmpNode, TreeNode root) {
+		if (root == null) {
+			tmpNode.sum = 0;
+			return;
+		}
+
+		Node left = new Node(0);
+		Node right = new Node(0);
+
+		helper(maxNode, left, root.left);
+		helper(maxNode, right, root.right);
+
+		tmpNode.sum = Math.max(root.val, Math.max(left.sum, right.sum) + root.val);
+		maxNode.sum = Math.max(maxNode.sum, Math.max(tmpNode.sum, root.val + left.sum + right.sum));
+	}
+
+	public int maxPathSum2(TreeNode root) {
 		if (root == null) {
 			return 0;
 		}
@@ -32,7 +86,7 @@ public class BinaryTreeMaximumPathSum {
 
 		maxPathSumHelper(maxSum, leftSum, node.left);
 		maxPathSumHelper(maxSum, rightSum, node.right);
-		
+
 		tmpSum.val = Math.max(node.val, Math.max(node.val + leftSum.val, node.val + rightSum.val));
 		maxSum.val = Math.max(maxSum.val, Math.max(tmpSum.val, node.val + leftSum.val + rightSum.val));
 	}
