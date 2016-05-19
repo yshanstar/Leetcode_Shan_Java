14aeebcfdb34c6cb3b7deeb73f7fa0ad7fe857a7
shye <shye@microsoft.com>
2016-05-19 09:20:49 -0700

    Sqrt(x)

--- Leetcode_Java/src/hack/leetcode/dev/Sqrt.java
+++ Leetcode_Java/src/hack/leetcode/dev/Sqrt.java
@@ -1,7 +1,30 @@
 package hack.leetcode.dev;
 
+/*
+ * Implement int sqrt(int x).
+ Compute and return the square root of x.
+ */
 public class Sqrt {
-	public int sqrt(int x) {
+	public int mySqrt(int x) {
+		if (x <= 0) {
+			return 0;
+		}
+
+		long low = 0;
+		long high = x;
+
+		while (high >= low) {
+			long mid = (high + low) / 2;
+			if (x < mid * mid) {
+				high = mid - 1;
+			} else {
+				low = mid + 1;
+			}
+		}
+		return (int) high;
+	}
+
+	public int sqrt2(int x) {
 		long lo = 0;
 		long hi = x;
 
