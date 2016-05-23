123e7d586899df05e860b1265d2628335adbc60b
Shan Ye <sye@dons.usfca.edu>
2016-05-22 23:03:43 -0700

    update comments

--- Leetcode_Java_3/src/solution/PalindromePairs.java
+++ Leetcode_Java_3/src/solution/PalindromePairs.java
@@ -17,6 +17,19 @@ Example 2:
 Given words = ["abcd", "dcba", "lls", "s", "sssll"]
 Return [[0, 1], [1, 0], [3, 2], [2, 4]]
 The palindromes are ["dcbaabcd", "abcddcba", "slls", "llssssll"]
+
+
+There are several cases to be considered that isPalindrome(s1 + s2):
+
+Case1: If s1 is a blank string, then for any string that is palindrome s2, s1+s2 and s2+s1 are palindrome.
+
+Case 2: If s2 is the reversing string of s1, then s1+s2 and s2+s1 are palindrome.
+
+Case 3: If s1[0:cut] is palindrome and there exists s2 is the reversing string of s1[cut+1:] , then s2+s1 is palindrome.
+
+Case 4: Similiar to case3. If s1[cut+1: ] is palindrome and there exists s2 is the reversing string of s1[0:cut] , then s1+s2 is palindrome.
+
+To make the search faster, build a HashMap to store the String-idx pairs.
  */
 public class PalindromePairs {
 	public List<List<Integer>> palindromePairs(String[] words) {
