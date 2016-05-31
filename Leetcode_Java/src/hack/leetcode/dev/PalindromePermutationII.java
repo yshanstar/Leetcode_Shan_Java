2154b8aa3d93a4058b25953ec8e6705b158aa22e
shye <shye@microsoft.com>
2016-05-31 15:34:36 -0700

    Palindrome Permutation II

--- Leetcode_Java/src/hack/leetcode/dev/PalindromePermutationII.java
+++ Leetcode_Java/src/hack/leetcode/dev/PalindromePermutationII.java
@@ -3,6 +3,7 @@ package hack.leetcode.dev;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /*
  * Given a string s, return all the palindromic permutations (without duplicates) of it. Return an empty list if no palindromic permutation could be form.
@@ -15,6 +16,64 @@ import java.util.List;
  */
 public class PalindromePermutationII {
 	public List<String> generatePalindromes(String s) {
+		if (s == null) {
+			throw new IllegalArgumentException("String is empty");
+		}
+
+		List<String> res = new ArrayList<String>();
+		if (s.isEmpty()) {
+			return res;
+		}
+
+		Map<Character, Integer> map = new HashMap<Character, Integer>();
+		for (char c : s.toCharArray()) {
+			if (map.containsKey(c)) {
+				map.put(c, map.get(c) + 1);
+			} else {
+				map.put(c, 1);
+			}
+		}
+
+		int oddCount = 0;
+		char oddChar = 'a';
+
+		for (char c : map.keySet()) {
+			if (map.get(c) % 2 == 1) {
+				oddCount++;
+				oddChar = c;
+			}
+		}
+
+		if (oddCount > 1) {
+			return res;
+		}
+
+		if (oddCount == 1) {
+			helper(map, oddChar + "", s.length(), res);
+		} else {
+			helper(map, "", s.length(), res);
+		}
+
+		return res;
+	}
+
+	private void helper(Map<Character, Integer> map, String tmp, int length, List<String> res) {
+		if (tmp.length() == length) {
+			res.add(tmp);
+			return;
+		}
+
+		for (char c : map.keySet()) {
+			if (map.get(c) >= 2) {
+				String cur = c + tmp + c;
+				map.put(c, map.get(c) - 2);
+				helper(map, cur, length, res);
+				map.put(c, map.get(c) + 2);
+			}
+		}
+	}
+
+	public List<String> generatePalindromes2(String s) {
 		if (s == null) {
 			throw new IllegalArgumentException("s is null");
 		}
@@ -57,8 +116,7 @@ public class PalindromePermutationII {
 		return res;
 	}
 
-	private void helper(HashMap<Character, Integer> map, String cur,
-			int target, List<String> res) {
+	private void helper(HashMap<Character, Integer> map, String cur, int target, List<String> res) {
 		String newCur = cur;
 		int len = newCur.length();
 
