8449d0ba90e7f5e31b26592254718963a19ef7c5
shye <shye@microsoft.com>
2016-05-31 16:09:11 -0700

    Permutations

--- Leetcode_Java_3/src/solution/Permutations.java
+++ Leetcode_Java_3/src/solution/Permutations.java
@@ -1,20 +1,96 @@
 package solution;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+/*
+ * Given a collection of distinct numbers, return all possible permutations.
+
+ For example,
+ [1,2,3] have the following permutations:
+ [1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1].
+ */
 public class Permutations {
-	public ArrayList<ArrayList<Integer>> permute(int[] num) {
+	public List<List<Integer>> permute(int[] nums) {
+		List<List<Integer>> res = new ArrayList<List<Integer>>();
+		if (nums == null || nums.length == 0) {
+			return res;
+		}
+
+		Set<Integer> numSet = new HashSet<Integer>();
+		for (int n : nums) {
+			numSet.add(n);
+		}
+		helper(numSet, nums.length, new ArrayList<Integer>(), res);
+
+		return res;
+	}
+
+	private void helper(Set<Integer> set, int size, List<Integer> tmpList, List<List<Integer>> res) {
+		if (tmpList.size() == size) {
+			res.add(new ArrayList<Integer>(tmpList));
+			return;
+		}
+
+		Set<Integer> copySet = new HashSet<Integer>(set);
+
+		for (Integer i : set) {
+			tmpList.add(i);
+			copySet.remove(i);
+			helper(copySet, size, tmpList, res);
+			tmpList.remove(tmpList.size() - 1);
+			copySet.add(i);
+		}
+	}
+
+	public static void main(String[] args) {
+		Permutations test = new Permutations();
+		int[] nums = new int[] { 0, 1 };
+		test.permute(nums);
+	}
+
+	public List<List<Integer>> permute3(int[] nums) {
+		List<List<Integer>> res = new ArrayList<List<Integer>>();
+		if (nums == null || nums.length == 0) {
+			return res;
+		}
+
+		boolean[] used = new boolean[nums.length];
+		helper(nums, new ArrayList<Integer>(), used, res);
+
+		return res;
+	}
+
+	private void helper(int[] nums, List<Integer> tmpList, boolean[] used, List<List<Integer>> res) {
+		if (tmpList.size() == nums.length) {
+			res.add(new ArrayList<Integer>(tmpList));
+			return;
+		}
+
+		for (int i = 0; i < nums.length; i++) {
+			if (!used[i]) {
+				used[i] = true;
+				tmpList.add(nums[i]);
+				helper(nums, tmpList, used, res);
+				tmpList.remove(tmpList.size() - 1);
+				used[i] = false;
+			}
+		}
+	}
+
+	public ArrayList<ArrayList<Integer>> permute2(int[] num) {
 		ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
 		if (num.length == 0) {
 			return res;
 		}
 		int[] used = new int[num.length];
-		helper(res, 0, num, new ArrayList<Integer>(), used);
+		helper2(res, 0, num, new ArrayList<Integer>(), used);
 		return res;
 	}
 
-	private void helper(ArrayList<ArrayList<Integer>> res, int idx, int[] num,
-			ArrayList<Integer> tmp, int[] used) {
+	private void helper2(ArrayList<ArrayList<Integer>> res, int idx, int[] num, ArrayList<Integer> tmp, int[] used) {
 		if (idx == num.length) {
 			res.add(new ArrayList<Integer>(tmp));
 			return;
@@ -24,7 +100,7 @@ public class Permutations {
 			if (used[i] == 0) {
 				used[i] = 1;
 				tmp.add(num[i]);
-				helper(res, idx + 1, num, tmp, used);
+				helper2(res, idx + 1, num, tmp, used);
 				used[i] = 0;
 				tmp.remove(tmp.size() - 1);
 			}
--- Leetcode_Java_3/src/solution/PermutationsII.java
+++ Leetcode_Java_3/src/solution/PermutationsII.java
@@ -7,12 +7,45 @@ import java.util.List;
 /*
  * Given a collection of numbers that might contain duplicates, return all possible unique permutations.
 
-For example,
-[1,1,2] have the following unique permutations:
-[1,1,2], [1,2,1], and [2,1,1].
+ For example,
+ [1,1,2] have the following unique permutations:
+ [1,1,2], [1,2,1], and [2,1,1].
  */
 public class PermutationsII {
 	public List<List<Integer>> permuteUnique(int[] nums) {
+		List<List<Integer>> res = new ArrayList<List<Integer>>();
+		if (nums == null || nums.length == 0) {
+			return res;
+		}
+
+		Arrays.sort(nums);
+		boolean[] used = new boolean[nums.length];
+		helper(used, nums, res, new ArrayList<Integer>());
+
+		return res;
+	}
+
+	private void helper(boolean[] used, int[] nums, List<List<Integer>> res, List<Integer> tmpList) {
+		if (tmpList.size() == nums.length) {
+			res.add(new ArrayList<Integer>(tmpList));
+			return;
+		}
+
+		for (int i = 0; i < nums.length; i++) {
+			if (!used[i]) {
+				used[i] = true;
+				tmpList.add(nums[i]);
+				helper(used, nums, res, tmpList);
+				used[i] = false;
+				tmpList.remove(tmpList.size() - 1);
+				while (i + 1 < nums.length && nums[i + 1] == nums[i]) {
+					i++;
+				}
+			}
+		}
+	}
+
+	public List<List<Integer>> permuteUnique2(int[] nums) {
 		List<List<Integer>> res = new ArrayList<List<Integer>>();
 		if (nums == null || nums.length == 0) {
 			res.add(new ArrayList<Integer>());
@@ -20,12 +53,12 @@ public class PermutationsII {
 		}
 		Arrays.sort(nums);
 		int[] used = new int[nums.length];
-		helper(nums, res, 0, new ArrayList<Integer>(), used);
+		helper2(nums, res, 0, new ArrayList<Integer>(), used);
 
 		return res;
 	}
 
-	private void helper(int[] nums, List<List<Integer>> res, int idx, List<Integer> tmpList, int[] used) {
+	private void helper2(int[] nums, List<List<Integer>> res, int idx, List<Integer> tmpList, int[] used) {
 		if (idx >= nums.length) {
 			res.add(new ArrayList<Integer>(tmpList));
 			return;
@@ -35,7 +68,7 @@ public class PermutationsII {
 			if (used[i] == 0) {
 				tmpList.add(nums[i]);
 				used[i] = 1;
-				helper(nums, res, idx + 1, new ArrayList<Integer>(tmpList), used.clone());
+				helper2(nums, res, idx + 1, new ArrayList<Integer>(tmpList), used.clone());
 				used[i] = 0;
 				tmpList.remove(tmpList.size() - 1);
 				while (i + 1 < nums.length && nums[i + 1] == nums[i]) {
