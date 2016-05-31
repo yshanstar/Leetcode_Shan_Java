f764e438ef9e4cfde680423bd26d1a6ad22aa585
shye <shye@microsoft.com>
2016-05-31 10:47:25 -0700

    Trapping Rain Water

--- Leetcode_Java_3/src/solution/TrappingRainWater.java
+++ Leetcode_Java_3/src/solution/TrappingRainWater.java
@@ -1,7 +1,42 @@
 package solution;
 
+/*
+ * Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
+
+ For example, 
+ Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
+ https://leetcode.com/discuss/63606/very-concise-java-solution-no-stack-with-explanations
+ */
 public class TrappingRainWater {
-	public int trap(int[] A) {
+	public static int trap(int[] height) {
+		if (height == null || height.length < 2) {
+			return 0;
+		}
+		int secHigh = 0;
+		int left = 0;
+		int right = height.length - 1;
+		int res = 0;
+		while (left < right) {
+			if (height[left] < height[right]) {
+				secHigh = Math.max(secHigh, height[left]);
+				res += secHigh - height[left];
+				left++;
+			} else {
+				secHigh = Math.max(secHigh, height[right]);
+				res += secHigh - height[right];
+				right--;
+			}
+		}
+
+		return res;
+	}
+
+	public static void main(String[] args) {
+		int[] water = new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+		trap(water);
+	}
+
+	public int trap2(int[] A) {
 		if (A.length == 0) {
 			return 0;
 		}
---
+++ Leetcode_Java_3/src/test/BitOperationTest.java
@@ -0,0 +1,51 @@
+package test;
+
+import java.util.BitSet;
+
+public class BitOperationTest {
+	public static void main(String[] args) {
+		TestBitSet();
+	}
+
+	public static void TestBitSet() {
+		BitSet set = new BitSet(1000);
+		for (int i = 0; i < 1000; i++) {
+			set.set(i);
+		}
+		System.out.print(set.cardinality());
+
+		for (int i = 0; i < 1000; i++) {
+			if (i % 5 != 0) {
+				set.set(i, false);
+			}
+		}
+
+		System.out.print(set);
+		System.out.print(set.cardinality());
+	}
+
+	public static long convertIPtoLong(String Ip) {
+		if (Ip == null || Ip.isEmpty()) {
+			return 0;
+		}
+
+		String[] segment = Ip.split("\\.");
+		long value = 0;
+		for (int i = 0; i < segment.length; i++) {
+			value = value << 8;
+			value |= Integer.parseInt(segment[i]);
+		}
+
+		return value;
+	}
+
+	public static long ipToLong(String ip) {
+		String[] address = ip.split("\\.");
+		long result = 0;
+		for (String octet : address) {
+			result <<= 8;
+			result |= Integer.parseInt(octet) & 0xff;
+		}
+		return result;
+	}
+}
