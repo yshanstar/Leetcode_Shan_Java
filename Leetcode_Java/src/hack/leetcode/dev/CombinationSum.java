16f39757583c754e59c2c61551cb3750382276af
Shan Ye <sye@dons.usfca.edu>
2016-06-01 21:18:44 -0700

    Combination Sum with DP

--- Leetcode_Java/src/hack/leetcode/dev/CombinationSum.java
+++ Leetcode_Java/src/hack/leetcode/dev/CombinationSum.java
@@ -2,7 +2,9 @@ package hack.leetcode.dev;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /*
  * Given a set of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
@@ -17,8 +19,49 @@ import java.util.List;
  A solution set is: 
  [7] 
  [2, 2, 3] 
+ 
+ http://blog.csdn.net/zyfo2/article/details/8592955
  */
 public class CombinationSum {
+	public List<List<Integer>> combinationSumDP(int[] candidates, int target) {
+		List<List<Integer>> res = new ArrayList<List<Integer>>();
+		if (candidates == null || candidates.length == 0 || target < 0) {
+			return res;
+		}
+
+		Arrays.sort(candidates);
+
+		Map<Integer, List<List<Integer>>> map = new HashMap<Integer, List<List<Integer>>>();
+		List<Integer> m = new ArrayList<Integer>();
+		List<List<Integer>> n = new ArrayList<List<Integer>>();
+		n.add(m);
+		map.put(0, n);
+
+		List<List<Integer>> a, b, c;
+
+		for (int i = 0; i < candidates.length; i++) {
+			for (int s = 1; s <= target; s++) {
+				if (s >= candidates[i] && map.containsKey(s - candidates[i])) {
+					a = new ArrayList<List<Integer>>();
+					c = map.get(s - candidates[i]);
+					for (List<Integer> x : c) {
+						List<Integer> tmp = new ArrayList<Integer>(x);
+						tmp.add(candidates[i]);
+						a.add(tmp);
+					}
+					if (map.containsKey(s)) {
+						b = map.get(s);
+						b.addAll(a);
+					} else {
+						map.put(s, a);
+					}
+				}
+			}
+		}
+
+		return map.get(target) == null ? res : map.get(target);
+	}
+
 	public List<List<Integer>> combinationSum(int[] candidates, int target) {
 		List<List<Integer>> res = new ArrayList<List<Integer>>();
 		if (candidates == null || candidates.length == 0 || target < 0) {
