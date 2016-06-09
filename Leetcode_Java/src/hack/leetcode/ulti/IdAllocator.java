8e35cb82b54fff58dd4be3212415709b5be37daa
Shan Ye <sye@dons.usfca.edu>
2016-06-09 08:09:17 -0700

    id allocator

---
+++ Leetcode_Java/src/hack/leetcode/ulti/IdAllocator.java
@@ -0,0 +1,60 @@
+package hack.leetcode.ulti;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+
+public class IdAllocator {
+	private static int x;
+	private static int max;
+	private static Queue<Integer> freeId;
+
+	public IdAllocator(int max) {
+		this.x = 0;
+		this.max = max;
+		this.freeId = new LinkedList<Integer>();
+	}
+
+	public static int alloc() {
+		if (freeId.isEmpty()) {
+			if (x == max) {
+				System.out.println("No id available");
+				return -1;
+			}
+			int id = x++;
+			return id;
+		} else {
+			int id = freeId.poll();
+			return id;
+		}
+	}
+
+	public static boolean free(int id) {
+		if (id >= x) {
+			System.out.println("id: " + id + " has not been assigned");
+			return false;
+		}
+
+		if (freeId.contains(id)) {
+			System.out.println("id: " + id + " has not been free before");
+			return false;
+		}
+
+		freeId.offer(id);
+		return true;
+	}
+
+	public static void main(String[] args) {
+		IdAllocator idAllocator = new IdAllocator(Integer.MAX_VALUE);
+		Random rm = new Random();
+		for (int i = 0; i < 15; i++) {
+			System.out.println("Assign Id : " + idAllocator.alloc());
+
+			if (i > 0 && (i % 5 == 0 || i % 3 == 0)) {
+				int id = rm.nextInt(i);
+				System.out.println("Free Id : " + id);
+				idAllocator.free(id);
+			}
+		}
+	}
+}
