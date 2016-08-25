b0caadfcae3c3f2df9c469c063b8f5f163c463b8
shye <shye@microsoft.com>
2016-08-25 11:21:07 -0700

    Ransom Note

--- Leetcode_Java/src/hack/leetcode/ulti/NestedInteger.java
+++ Leetcode_Java/src/hack/leetcode/ulti/NestedInteger.java
@@ -2,19 +2,34 @@ package hack.leetcode.ulti;
 
 import java.util.List;
 
-public interface NestedInteger {
+public abstract class NestedInteger {
+
+	// Constructor initializes an empty nested list.
+	public NestedInteger() {
+	}
+
+	// Constructor initializes a single integer.
+	public NestedInteger(int value) {
+	}
 
 	// @return true if this NestedInteger holds a single integer, rather than a
 	// nested list.
-	public boolean isInteger();
+	public abstract boolean isInteger();
 
 	// @return the single integer that this NestedInteger holds, if it holds a
 	// single integer
 	// Return null if this NestedInteger holds a nested list
-	public Integer getInteger();
+	public abstract Integer getInteger();
+
+	// Set this NestedInteger to hold a single integer.
+	public abstract void setInteger(int value);
+
+	// Set this NestedInteger to hold a nested list and adds a nested integer to
+	// it.
+	public abstract void add(NestedInteger ni);
 
 	// @return the nested list that this NestedInteger holds, if it holds a
 	// nested list
 	// Return null if this NestedInteger holds a single integer
-	public List<NestedInteger> getList();
+	public abstract List<NestedInteger> getList();
 }
